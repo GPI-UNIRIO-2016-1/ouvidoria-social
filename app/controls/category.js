9f26aaf076bd6c88fba33e20d821a6b96f2d4a6d
Luis Oliveira <luisoliveiras14@gmail.com>
2016-05-10 04:19:55 -0300

    Configuração das paginas de categoria.

---
+++ app/controls/category.js
@@ -0,0 +1,57 @@
+﻿
+
+var Category = require('../models/category')
+var ObjectId = require('mongoose').Types.ObjectId;
+
+var methods = {};
+
+methods.register = {};
+
+methods.register.get = function (req, res, next) {
+    res.render('../views/category/form', {});
+};
+
+
+methods.register.post = function (req, res, next) {
+    var category = new Category();
+
+    category.name = req.body.name
+    category.description = req.body.description
+
+    category.save(function (err) {
+        if (err) {
+            console.log(err);
+            return res.render('../views/category/form', {});
+        }
+
+        req.flash("success", "Incluido com sucesso.");
+        res.redirect("/user/list");
+    })
+
+};
+
+
+methods.view = function (req, res, next) {
+    var categoryName = req.params.name;
+
+    Category.findOne({ name: categoryName }, function (err, doc) {
+        if (err)
+            return next(err);
+
+        res.render('../views/category/view', { viewCategory: doc });
+    });
+};
+
+
+methods.list = function (req, res, next) {
+    Category.find({}, function (err, categories) {
+        if (err)
+            return next(err);
+
+        console.log(categories);
+
+        res.render('../views/category/list', { categories: categories });
+    });
+};
+
+module.exports = methods;
